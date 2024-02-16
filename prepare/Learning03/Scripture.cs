0da4b1557f8cfe7d3433eeae1fd3dd6845d6f528
Samuel-Benson <samuelgbens@gmail.com>
2024-02-16 15:53:17 -0700

    Learning 03

---
+++ prepare/Learning03/Scripture.cs
@@ -0,0 +1,11 @@
+using System;
+using System.Runtime.CompilerServices;
+
+public class Scripture
+{
+    private string reference;
+    private List<Word>;
+    private int numBlankWords;
+
+    private
+}
